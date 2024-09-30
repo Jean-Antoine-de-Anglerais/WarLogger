9aefa58572ef0ab4c461cd993a27cc9957615d6e
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-09-30 21:19:22 +0300

    Code refactoring

--- WarLogger_BepInEx_Preloader/CustomWarData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WarLogger_BepInEx_Preloader
-{
-    // public class CustomWarData
-    // {
-    //     public int id = 5;
-    // }
-}
--- WarLogger_BepInEx_Preloader/Patcher.cs
+++ WarLogger_BepInEx_Preloader/Patcher.cs
@@ -8,41 +8,17 @@ namespace WarLogger_BepInEx_Preloader
 {
     public static class Patcher
     {
-        // List of assemblies to patch
         public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
 
-        // Patches the assemblies
         public static void Patch(AssemblyDefinition assembly)
         {
-            // Patcher code here
-
             ModuleDefinition mainModule = assembly.MainModule;
 
             var customWarData = new TypeDefinition("", "CustomWarData", 
                 TypeAttributes.Class | TypeAttributes.Public, 
                 mainModule.ImportReference(typeof(object)));
 
-            Console.WriteLine(customWarData.Methods.Count);
-
-            // Create a constructor
-            var customWarDataConstructor = new MethodDefinition(
-                ".ctor",
-                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
-                mainModule.ImportReference(typeof(void)));
-
-            // Generate IL code for the constructor
-            var customWarDataIlProcessor = customWarDataConstructor.Body.GetILProcessor();
-
-            // Start of the method
-            customWarDataIlProcessor.Emit(OpCodes.Ldarg_0); // Загружаем ссылку на текущий объект (this)
-
-            // Call the base constructor (object)
-            customWarDataIlProcessor.Emit(OpCodes.Call, mainModule.ImportReference(typeof(object).GetConstructor(Type.EmptyTypes)));
-
-            // End the method
-            customWarDataIlProcessor.Emit(OpCodes.Ret);
-
-            customWarData.Methods.Add(customWarDataConstructor);  // Add a constructor to the class
+            CustomWarDataClassMaking(customWarData, mainModule);
 
             mainModule.Types.Add(customWarData);
 
@@ -54,34 +30,28 @@ namespace WarLogger_BepInEx_Preloader
 
             warClass.Fields.Add(newField);
 
-            // Add initialization in the constructor
             var warConstructor = warClass.Methods.FirstOrDefault(m => m.Name == ".ctor");
 
-            if (warConstructor != null)
-            {
-                var ilProcessor = warConstructor.Body.GetILProcessor();
-                ilProcessor.InsertBefore(warConstructor.Body.Instructions[0], Instruction.Create(OpCodes.Ldarg_0));
-                ilProcessor.InsertBefore(warConstructor.Body.Instructions[1], Instruction.Create(OpCodes.Newobj, mainModule.ImportReference(customWarData.Resolve().Methods.First(m => m.Name == ".ctor"))));
-                ilProcessor.InsertBefore(warConstructor.Body.Instructions[2], Instruction.Create(OpCodes.Stfld, newField));
-            }
+            var ilProcessor = warConstructor.Body.GetILProcessor();
+            ilProcessor.InsertBefore(warConstructor.Body.Instructions[0], Instruction.Create(OpCodes.Ldarg_0));
+            ilProcessor.InsertBefore(warConstructor.Body.Instructions[1], Instruction.Create(OpCodes.Newobj, mainModule.ImportReference(customWarData.Resolve().Methods.First(m => m.Name == ".ctor"))));
+            ilProcessor.InsertBefore(warConstructor.Body.Instructions[2], Instruction.Create(OpCodes.Stfld, newField));   
         }
 
+        private static void CustomWarDataClassMaking(TypeDefinition customWarData, ModuleDefinition mainModule)
+        {
+            var customWarDataConstructor = new MethodDefinition(
+                ".ctor",
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
+                mainModule.ImportReference(typeof(void)));
 
-        // public static void PrintTypes(string fileName)
-        // {
-        //     fileName = @"C:\Program Files (x86)\Steam\steamapps\common\worldbox\worldbox_Data\Managed\" + fileName + ".dll";
-        // 
-        //     ModuleDefinition module = ModuleDefinition.ReadModule(fileName);
-        // 
-        //     foreach (TypeDefinition type in module.Types)
-        //     {
-        //         if (!type.IsPublic)
-        //         {
-        //             continue;
-        //         }
-        // 
-        //         Console.WriteLine(type.FullName);
-        //     }
-        // }
+            var customWarDataIlProcessor = customWarDataConstructor.Body.GetILProcessor();
+
+            customWarDataIlProcessor.Emit(OpCodes.Ldarg_0); 
+            customWarDataIlProcessor.Emit(OpCodes.Call, mainModule.ImportReference(typeof(object).GetConstructor(Type.EmptyTypes)));
+            customWarDataIlProcessor.Emit(OpCodes.Ret);
+
+            customWarData.Methods.Add(customWarDataConstructor);
+        }
     }
 }
