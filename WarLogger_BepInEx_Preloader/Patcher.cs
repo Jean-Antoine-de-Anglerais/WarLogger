bdd0c1ffce0ef6d770e37a499c86e2bdf4c0b5c6
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-09-30 23:15:53 +0300

    Continued code refactoring

--- WarLogger_BepInEx_Preloader/Patcher.cs
+++ WarLogger_BepInEx_Preloader/Patcher.cs
@@ -3,6 +3,7 @@ using Mono.Cecil.Cil;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using WarLogger_Helper;
 
 namespace WarLogger_BepInEx_Preloader
 {
@@ -14,17 +15,9 @@ namespace WarLogger_BepInEx_Preloader
         {
             ModuleDefinition mainModule = assembly.MainModule;
 
-            var customWarData = new TypeDefinition("", "CustomWarData", 
-                TypeAttributes.Class | TypeAttributes.Public, 
-                mainModule.ImportReference(typeof(object)));
-
-            CustomWarDataClassMaking(customWarData, mainModule);
-
-            mainModule.Types.Add(customWarData);
-
             var warClass = mainModule.Types.FirstOrDefault(t => t.Name == "War");
 
-            var warData = mainModule.ImportReference(customWarData);
+            var warData = mainModule.ImportReference(typeof(CustomWarData));
 
             var newField = new FieldDefinition("warData", FieldAttributes.Public, warData);
 
@@ -34,24 +27,8 @@ namespace WarLogger_BepInEx_Preloader
 
             var ilProcessor = warConstructor.Body.GetILProcessor();
             ilProcessor.InsertBefore(warConstructor.Body.Instructions[0], Instruction.Create(OpCodes.Ldarg_0));
-            ilProcessor.InsertBefore(warConstructor.Body.Instructions[1], Instruction.Create(OpCodes.Newobj, mainModule.ImportReference(customWarData.Resolve().Methods.First(m => m.Name == ".ctor"))));
-            ilProcessor.InsertBefore(warConstructor.Body.Instructions[2], Instruction.Create(OpCodes.Stfld, newField));   
-        }
-
-        private static void CustomWarDataClassMaking(TypeDefinition customWarData, ModuleDefinition mainModule)
-        {
-            var customWarDataConstructor = new MethodDefinition(
-                ".ctor",
-                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
-                mainModule.ImportReference(typeof(void)));
-
-            var customWarDataIlProcessor = customWarDataConstructor.Body.GetILProcessor();
-
-            customWarDataIlProcessor.Emit(OpCodes.Ldarg_0); 
-            customWarDataIlProcessor.Emit(OpCodes.Call, mainModule.ImportReference(typeof(object).GetConstructor(Type.EmptyTypes)));
-            customWarDataIlProcessor.Emit(OpCodes.Ret);
-
-            customWarData.Methods.Add(customWarDataConstructor);
+            ilProcessor.InsertBefore(warConstructor.Body.Instructions[1], Instruction.Create(OpCodes.Newobj, mainModule.ImportReference(warData.Resolve().Methods.First(m => m.Name == ".ctor"))));
+            ilProcessor.InsertBefore(warConstructor.Body.Instructions[2], Instruction.Create(OpCodes.Stfld, newField));
         }
     }
 }
